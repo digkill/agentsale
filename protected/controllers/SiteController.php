d9184fdee1853b24dfa8210ed3c47e04934a5763
votinov.mn <votinov.mn@UK-ZHAKOV-V.corp.ertelecom.loc>
2015-10-21 11:55:43 +0500

    WEB-2666

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -44,7 +44,7 @@ class SiteController extends Controller {
         ];
     }
 
-    public function actions() {
+    public function actions() { 
         return [
             'error' => [
                 'class' => 'yii\web\ErrorAction',
